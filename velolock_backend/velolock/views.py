042fa3cb290893eae2d3f24dbe18bc6d33ea2eeb
sand0rka <115060928+sand0rka@users.noreply.github.com>
2023-05-02 18:20:34 +0300

    add all files

---
+++ velolock_backend/velolock/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+import bluetooth
+
+
+def open_lock(request):
+    addr = '00:00:00:00:00:00'  # Замініть на MAC-адресу вашого Arduino
+    port = 1  # Замініть на відповідний порт Bluetooth на вашому Arduino
+
+    try:
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((addr, port))
+        sock.send('1')  # Відправити команду відкриття замка
+        sock.close()
+        return HttpResponse('Lock opened successfully!')
+    except:
+        return HttpResponse('Failed to open lock!')
+
+
+def close_lock(request):
+    addr = '00:00:00:00:00:00'  # Замініть на MAC-адресу вашого Arduino
+    port = 1  # Замініть на відповідний порт Bluetooth на вашому Arduino
+
+    try:
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((addr, port))
+        sock.send('0')  # Відправити команду закриття замка
+        sock.close()
+        return HttpResponse('Lock closed successfully!')
+    except:
+        return HttpResponse('Failed to close lock!')
---
+++ velolock_backend/velolock_backend/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for velolock_backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from velolock_backend.views import open_lock, close_lock
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('open-lock/', views.open_lock, name='open_lock'),
+    path('close-lock/', views.close_lock, name='close_lock'),
+]
